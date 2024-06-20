f1bdd3a11cfdeb830ef77aa8a7b08c7d3b02a651
chanhee060710 <ppp940304ppp@gmail.com>
2024-06-20 14:03:43 +0900

    수박게임

---
+++ main.js
@@ -0,0 +1,48 @@
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    World = Matter.World;
+
+//엔진 선언 
+const engine = Engine.create();
+
+//렌더 선언
+const render = Render.create({
+    engine,
+    element: document.body,
+    options: {
+        wireframes: false, //true면 색적용 안됨
+        background: '#F7F8C8',
+        width: 620,
+        height: 850
+    }
+});
+
+const world = engine.world;
+
+const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+    isStatic: true,
+    render: {fillStyle:'#E6B143'}
+})
+
+const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+    isStatic: true,
+    render: {fillStyle:'#E6B143'}
+})
+
+const ground = Bodies.rectangle(310, 820, 620, 60, {
+    isStatic: true,
+    render: {fillStyle:'#E6B143'}
+})
+
+const topLine = Bodies.rectangle(310, 150, 620, 2, {
+    isStatic: true,
+    render: {fillStyle:'#E6B143'}
+})
+
+World.add(world, [leftWall,rightWall,ground,topLine])
+
+Render.run(render);
+Runner.run(engine);
+
